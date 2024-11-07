54d3242a36e3f454e2a8a70a9d825da70a0ca8ee
StechClarin <stechclarin@gmail.com>
2024-11-07 16:10:24 +0000

    recup

--- CompteBancaireTests/CompteBancaireTests.cs
+++ CompteBancaireTests/CompteBancaireTests.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using CompteBanqueNS;
 
-namespace BanqueTests
+namespace CompteBancaireTests
 {
     [TestClass]
     public class CompteBancaireTests
@@ -10,15 +9,6 @@ namespace BanqueTests
         [TestMethod]
         public void TestMethod1()
         {
-            double soldeInitial = 500000;
-            double montantDébit = 400000;
-            double soldeAttendu = 100000;
-            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
-
-            compte.Débiter(montantDébit);
-
-            double soldeObtenu = compte.Balance;
-            Assert.AreEqual(soldeAttendu, soldeObtenu, 0.001, "debit incorrect");
         }
     }
 }
