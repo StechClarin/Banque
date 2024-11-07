0afc61b44c68d6242e71b65185068e34e13a185e
StechClarin <stechclarin@gmail.com>
2024-11-07 16:41:21 +0000

    dernier test

--- CompteBancaireTests/CompteBancaireTests.cs
+++ CompteBancaireTests/CompteBancaireTests.cs
@@ -1,14 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using CompteBanqueNS;
 
-namespace CompteBancaireTests
+namespace BanqueTests
 {
     [TestClass]
     public class CompteBancaireTests
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DébiterMontantNégatifLèveArgumentOutOfRange()
         {
+            // Arrange
+            double soldeInitial = 500000;
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+            double montantADébiter = -100; // Montant négatif
+
+            // Act
+            compte.Débiter(montantADébiter);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DébiterMontantSupérieurSoldeLèveArgumentOutOfRange()
+        {
+            // Arrange
+            double soldeInitial = 500000;
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+            double montantADébiter = 600000; // Montant supérieur au solde
+
+            // Act
+            compte.Débiter(montantADébiter);
+        }
+
+        [TestMethod]
+        public void TestDébiterMontantValide()
+        {
+            double soldeInitial = 500000;
+            double montantDébit = 400000;
+            double soldeAttendu = 100000;
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+
+            compte.Débiter(montantDébit);
+
+            double soldeObtenu = compte.Balance;
+            Assert.AreEqual(soldeAttendu, soldeObtenu, 0.001, "Débit incorrect");
         }
     }
+
 }
